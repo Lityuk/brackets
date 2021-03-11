f097a1c9f5ded3c51fbcaa79c668977856f639be
Lityuk <lityuk@gmail.com>
2021-03-12 02:40:49 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = bracketsConfig.map(function (item) {
+        return item[0] + item[1];
+    });
+
+    // Переделано без рекрсии
+
+    while (str.length > 0) {
+        let newStr = str;
+        arr.forEach((element) => {
+            newStr = newStr.replace(element, "");
+        });
+        if (newStr === str) {
+            return false;
+        }
+        str = newStr;
+    }
+    return true;
+
+    // Все проверки вручную проходят, но команда npm test ругается и говорит что есть ошибки.
+    // Причина пока не ясна
+
+    // function remove() {
+    //     let newStr = str;
+    //     arr.forEach((element) => {
+    //       newStr = newStr.replace(element, "");
+    //       // console.log(newStr);
+    //     });
+
+    //     if (newStr === str) {
+    //       console.log("false");
+    //       return false;
+    //     } else if (newStr.length > 0) {
+    //       str = newStr;
+    //       remove();
+    //     } else {
+    //       console.log("true");
+    //       return true;
+    //     }
+    //   }
+    //   remove();
+};
